c1f65a264f5b51d4713508891326d6ad762bd563
OksanaS13 <oksan4ik_96@mail.ru>
2023-04-10 19:50:42 +0300

    add src/locales/

---
+++ src/locales/ru.js
@@ -0,0 +1,11 @@
+export default {
+  translation: {
+    feedback: {
+      succes: 'RSS успешно загружен',
+      errors: {
+        invalidUrl: 'Ссылка должна быть валидным URL',
+        existingRss: 'RSS уже существует',
+      }
+    }
+  }
+};
