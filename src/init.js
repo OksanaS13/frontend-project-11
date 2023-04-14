67c22fdd111d246ffd52a5d484cd3c443ba1cab8
OksanaS13 <oksan4ik_96@mail.ru>
2023-04-14 16:26:26 +0300

    update src/init.js

--- src/init.js
+++ src/init.js
@@ -4,6 +4,7 @@ import onChange from 'on-change';
 import i18n from 'i18next';
 import render from './view.js';
 import ru from './locales/ru.js';
+import parse from './utils/parse.js';
 
 const validate = (url, addedUrls, i18nInstance) => {
   yup.setLocale({
@@ -18,6 +19,12 @@ const validate = (url, addedUrls, i18nInstance) => {
   return schema.validate(url);
 };
 
+const getHtml = (url) => fetch(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
+  .then((response) => {
+    if (response.ok) return response.json();
+    throw new Error('Network response was not ok.');
+  });
+
 export default () => {
   const i18nInstance = i18n.createInstance();
   i18nInstance.init({
@@ -31,7 +38,6 @@ export default () => {
   // Model
   const initialState = {
     form: {
-      status: 'filling',
       error: null,
     },
     rssLinks: [],
@@ -47,21 +53,30 @@ export default () => {
   };
 
   // View
-  const watchedState = onChange(initialState, (path, value) => {
-    render(path, value, elements, i18nInstance);
+  const watchedState = onChange(initialState, (path) => {
+    render(path, watchedState, elements, i18nInstance);
   });
 
   // Control
   elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    const url = formData.get('url');
-    validate(url.trim(), watchedState.rssLinks, i18nInstance)
-      .then((data) => {
-        watchedState.rssLinks.push(data);
-      })
+    const url = formData.get('url').trim();
+    validate(url, watchedState.rssLinks, i18nInstance)
+      .then((validUrl) => getHtml(validUrl))
       .catch((data) => {
         watchedState.form.error = data.errors.join('');
+      })
+      .then(({ contents }) => {
+        const parser = new DOMParser();
+        const xmlString = parser.parseFromString(contents, 'text/xml');
+        const rss = parse(xmlString);
+        if (!rss) {
+          watchedState.form.error = i18nInstance.t('feedback.errors.noRss');
+        }
+        watchedState.feeds.push(rss.feed);
+        watchedState.posts.unshift(...rss.posts);
+        watchedState.rssLinks.push(url);
       });
   });
 };
