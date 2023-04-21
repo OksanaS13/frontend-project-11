1f17813a50a82f9da5d768b3ed97b3c7acebf169
OksanaS13 <oksan4ik_96@mail.ru>
2023-04-21 17:23:01 +0300

    update src/view.js

--- src/view.js
+++ src/view.js
@@ -35,63 +35,91 @@ const addNewFeed = (feed) => {
   return item;
 };
 
-const addNewPosts = (posts) => posts.map((post) => {
+const addNewPosts = (posts, touchedPosts) => posts.map((post) => {
+  const { postTitle, link, postId } = post;
+  const touchedPostsId = touchedPosts.map((touchedPost) => touchedPost.postId);
+
   const item = document.createElement('li');
   item.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
 
   const itemTitle = document.createElement('a');
-  itemTitle.textContent = post.postTitle;
-  itemTitle.classList.add('fw-bold');
-  itemTitle.href = post.link;
-  itemTitle.setAttribute('data-id', post.id);
-  itemTitle.setAttribute('target', '_blank');
-  itemTitle.setAttribute('rel', 'noopener noreferrer');
-
   const button = document.createElement('button');
-  button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
-  button.textContent = 'Просмотр';
-  button.setAttribute('type', 'button');
-  button.setAttribute('data-id', post.id);
-  button.setAttribute('data-bs-toggle', 'modal');
-  button.setAttribute('data-bs-target', '#modal');
 
   item.append(itemTitle, button);
+  if (touchedPostsId.includes(postId)) {
+    itemTitle.outerHTML = `<a class="fw-normal" href="${link}" data-id="${postId}" target="_blank" rel="noopener noreferrer">${postTitle}</a>`;
+  } else {
+    itemTitle.outerHTML = `<a class="fw-bold" href="${link}" data-id="${postId}" target="_blank" rel="noopener noreferrer">${postTitle}</a>`;
+  }
+  button.outerHTML = `<button type="button" class="btn btn-outline-primary btn-sm" data-id="${postId}" data-bs-toggle="modal" data-bs-target="#modal">Просмотр</button>`;
+
   return item;
 });
 
-const render = (path, watchedState, elements, i18nInstance) => {
-  const input = elements.form.querySelector('input');
-
-  switch (path) {
-    case 'form.error': {
-      input.classList.add('is-invalid');
+const render = (path, value, watchedState, elements, i18nInstance) => {
+  switch (value) {
+    case 'filling': {
+      elements.form.input.removeAttribute('readonly');
+      elements.form.button.removeAttribute('disabled');
+      break;
+    }
+    case 'sending': {
+      elements.form.input.setAttribute('readonly', 'true');
+      elements.form.button.setAttribute('disabled', '');
+      break;
+    }
+    case 'failed': {
+      elements.form.input.classList.add('is-invalid');
       elements.feedback.classList.add('text-danger');
       elements.feedback.classList.remove('text-success');
       const { error } = watchedState.form;
       const text = document.createTextNode(error);
       elements.feedback.replaceChildren(text);
+
+      elements.form.input.removeAttribute('readonly');
+      elements.form.button.removeAttribute('disabled');
       break;
     }
-    case 'feeds': {
-      elements.feeds.replaceChildren(createBlock('Фиды'));
-      const currentFeed = _.last(watchedState.feeds);
-      const li = addNewFeed(currentFeed);
-      elements.feeds.querySelector('ul').append(li);
-      break;
-    }
-    case 'posts': {
-      elements.posts.replaceChildren(createBlock('Посты'));
-      const list = addNewPosts(watchedState.posts);
-      elements.posts.querySelector('ul').replaceChildren(...list);
-      break;
-    }
-    case 'rssLinks': {
-      input.classList.remove('is-invalid');
+    case 'processed': {
+      elements.form.input.classList.remove('is-invalid');
       elements.feedback.classList.remove('text-danger');
       elements.feedback.classList.add('text-success');
       const text = document.createTextNode(i18nInstance.t('feedback.succes'));
       elements.feedback.replaceChildren(text);
-      elements.form.reset();
+
+      elements.feeds.replaceChildren(createBlock('Фиды'));
+      const feeds = watchedState.feeds.map(addNewFeed);
+      elements.feeds.querySelector('ul').append(...feeds);
+
+      elements.posts.replaceChildren(createBlock('Посты'));
+      const list = addNewPosts(watchedState.posts, watchedState.uiState.touchedPosts);
+      elements.posts.querySelector('ul').replaceChildren(...list);
+
+      elements.form.container.reset();
+      break;
+    }
+    case 'show description': {
+      elements.modal.classList.add('show');
+      elements.modal.removeAttribute('aria-hidden');
+      elements.modal.setAttribute('aria-modal', 'true');
+      elements.modal.setAttribute('style', 'display: block;');
+
+      const modalTitle = elements.modal.querySelector('.modal-title');
+      const modalBody = elements.modal.querySelector('.modal-body');
+      const modalLink = elements.modal.querySelector('.full-article');
+
+      const { postTitle, postDescription, link } = _.last(watchedState.uiState.touchedPosts);
+
+      modalTitle.textContent = postTitle;
+      modalBody.textContent = postDescription;
+      modalLink.href = link;
+      break;
+    }
+    case 'touched post': {
+      const { postId } = _.last(watchedState.uiState.touchedPosts);
+      const currentPost = document.querySelector(`a[data-id="${postId}"]`);
+      currentPost.classList.remove('fw-bold');
+      currentPost.classList.add('fw-normal');
       break;
     }
     default: {
