a628a1d478b2d9ac781301b61872a8e59fb9b81a
OksanaS13 <oksan4ik_96@mail.ru>
2023-04-28 21:14:30 +0300

    add src/parse.js

---
+++ src/parse.js
@@ -0,0 +1,31 @@
+export default (contents) => {
+  const parser = new DOMParser();
+  const xmlString = parser.parseFromString(contents, 'text/xml');
+  const errorNode = xmlString.querySelector('parsererror');
+
+  if (errorNode) {
+    throw new Error('noRss');
+  } else {
+    const title = xmlString.querySelector('title');
+    const description = xmlString.querySelector('description');
+
+    const feed = {
+      title: title.textContent,
+      description: description.textContent,
+    };
+
+    const posts = [...xmlString.querySelectorAll('item')]
+      .map((item) => {
+        const postTitle = item.querySelector('title');
+        const postDescription = item.querySelector('description');
+        const link = item.querySelector('link');
+        return {
+          title: postTitle.textContent,
+          description: postDescription.textContent,
+          link: link.textContent,
+        };
+      });
+
+    return { feed, posts };
+  }
+};
